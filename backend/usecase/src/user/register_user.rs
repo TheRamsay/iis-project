ff8047568a715d7318e1b202ec71c6714530924a
TheRamsay <ramsayofbolton@gmail.com>
2024-11-23 01:28:22 +0100

    ✨ Registration unique

--- backend/usecase/src/user/register_user.rs
+++ backend/usecase/src/user/register_user.rs
@@ -14,6 +14,7 @@ use models::{
 
 use repository::{user_repository::UserRepository, wall_repository::WallRepository};
 use uuid::Uuid;
+use validator::{Validate, ValidationError, ValidationErrors};
 
 use super::auth_utils::hash_password;
 
@@ -53,31 +54,9 @@ where
     }
 
     pub async fn execute(&self, input: RegisterUserInput) -> AppResult<RegisterUserOutput> {
-        if let Some(u) = self
-            .user_repository
-            .get_by_username(input.username.clone())
-            .await?
-        {
-            return Err(AppError::Conflict(format!(
-                "User with username {} already exists",
-                u.username
-            )));
-        }
-
-        if let Some(u) = self
-            .user_repository
-            .get_by_email(input.email.clone())
-            .await?
-        {
-            return Err(AppError::Conflict(format!(
-                "User with email {} already exists",
-                u.email
-            )));
-        }
-
         let wall_id = self.wall_repository.create(Wall { id: Id::gen() }).await?;
 
-        let user = User::new(
+        let model = User::new(
             input.display_name,
             input.username,
             input.email,
@@ -87,8 +66,48 @@ where
             hash_password(&input.password)?,
         )?;
 
+        let mut validation_errors = ValidationErrors::new();
+
+        // Validate the model for domain rules
+        match model.validate() {
+            Ok(_) => (),
+            Err(e) => {
+                validation_errors = e;
+            }
+        }
+
+        // Check for unique constraints
+        if let Some(u) = self
+            .user_repository
+            .get_by_username(model.username.clone())
+            .await?
+        {
+            if u.id != model.id {
+                let mut validation_error = ValidationError::new("username");
+                validation_error = validation_error.with_message("Username already exists".into());
+                validation_error.add_param("value".into(), &model.username);
+                validation_errors.add("username", validation_error);
+            }
+        }
+
+        if let Some(u) = self
+            .user_repository
+            .get_by_email(model.email.clone())
+            .await?
+        {
+            if u.id != model.id {
+                let mut validation_error = ValidationError::new("email");
+                validation_error = validation_error.with_message("Email already exists".into());
+                validation_error.add_param("value".into(), &model.username);
+                validation_errors.add("email", validation_error);
+            }
+        }
+
+        if !validation_errors.is_empty() {
+            return Err(validation_errors.into());
+        }
         Ok(RegisterUserOutput {
-            id: self.user_repository.create(user).await?.id,
+            id: self.user_repository.create(model).await?.id,
         })
     }
 }
