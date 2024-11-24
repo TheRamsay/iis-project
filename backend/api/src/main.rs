f4589c7f2b99d26cb0f5ecfcc82430d267cad63f
TheRamsay <ramsayofbolton@gmail.com>
2024-11-24 21:40:49 +0100

    ✨ Two mains

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -28,6 +28,7 @@ use routes::user::user_routes;
 use routes::wall::wall_routes;
 use sea_orm::*;
 use sea_orm::{Database, DatabaseConnection};
+use shuttle_axum::AxumService;
 use tower::ServiceBuilder;
 use tower_http::cors::{AllowOrigin, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -56,12 +57,10 @@ pub struct AppState {
     pub redis_client: Arc<redis::Client>,
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn create_app_state() -> AppState {
     dotenv().ok();
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
     let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET is not set");
-    let frontend_url = std::env::var("FRONTEND_URL").expect("FRONTEND_URL is not set");
 
     let conn = Database::connect(db_url)
         .await
@@ -69,7 +68,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
 
     Migrator::up(&conn, None).await.expect("Migration failed");
 
-    let app_state = AppState {
+    AppState {
         user_repository: DbUserRepository::new(Arc::new(conn.clone())),
         group_repository: DbGroupRepository::new(Arc::new(conn.clone())),
         wall_repository: DbWallRepository::new(Arc::new(conn.clone())),
@@ -84,10 +83,13 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         conn: conn.clone(),
         jwt_secret,
         redis_client: Arc::new(redis::Client::open("redis://localhost:6379").unwrap()),
-        // redis_client: Arc::new(redis::Client::open("rediss://default:AWEgAAIjcDEzNmU1YzI0MzU1MGI0NThhOGM5N2Y4M2VlMDNjYjkxMnAxMA@sincere-mink-24864.upstash.io:6379").unwrap()),
-    };
+    }
+}
 
-    let router = Router::new()
+fn create_router(app_state: AppState) -> Router {
+    let frontend_url = std::env::var("FRONTEND_URL").expect("FRONTEND_URL is not set");
+
+    Router::new()
         .nest("/api/users", user_routes())
         .nest("/api/groups", group_routes())
         .nest("/api/auth", auth_routes())
@@ -113,46 +115,27 @@ async fn main() -> shuttle_axum::ShuttleAxum {
                 )
                 .into_inner(),
         )
-        .with_state(app_state);
+        .with_state(app_state)
+}
+
+#[cfg(feature = "shuttle")]
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let app_state = create_app_state().await;
+    let router = create_router(app_state);
 
     Ok(router.into())
 }
 
-// #[tokio::main]
-// async fn main() {
-//     dotenv().ok();
-//     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-//     let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET is not set");
+#[cfg(feature = "tokio")]
+#[tokio::main]
+async fn main() {
+    let app_state = create_app_state().await;
+    let router = create_router(app_state);
 
-//     let conn = Database::connect(db_url)
-//         .await
-//         .expect("Database connection failed");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
 
-//     Migrator::up(&conn, None).await.expect("Migration failed");
-
-//     let app_state = AppState {
-//         user_repository: DbUserRepository::new(Arc::new(conn.clone())),
-//         group_repository: DbGroupRepository::new(Arc::new(conn.clone())),
-//         wall_repository: DbWallRepository::new(Arc::new(conn.clone())),
-//         group_member_repository: DbGroupMemberRepository::new(Arc::new(conn.clone())),
-//         group_join_request_repository: DbGroupJoinRequestRepository::new(Arc::new(conn.clone())),
-//         post_repository: DbPostRepository::new(Arc::new(conn.clone())),
-//         cloudinary_repository: GenericRepository {},
-//         conn: conn.clone(),
-//         jwt_secret,
-//         redis_client: Arc::new(redis::Client::open("redis://localhost:6379").unwrap()),
-//     };
-
-//     let router = Router::new()
-//         .nest("/api/users", user_routes())
-//         .nest("/api/groups", group_routes())
-//         .nest("/api/auth", auth_routes())
-//         .nest("/api/group-join-requests", group_join_request_router())
-//         .nest("/api/posts", post_routes())
-//         .with_state(app_state);
-
-//     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-//         .await
-//         .unwrap();
-//     axum::serve(listener, router).await.unwrap();
-// }
+    axum::serve(listener, router).await.unwrap();
+}
