f47a3b187c62cc15d7fd673ecef5831e25407b26
LufyCZ <me@lufy.cz>
2024-11-26 14:18:38 +0000

    asdasdas

--- frontend/src/app/(auth)/logout/route.ts
+++ frontend/src/app/(auth)/logout/route.ts
@@ -7,6 +7,6 @@ export function GET() {
   response.headers.set("Location", "/");
   response.headers.set("Allow-Credentials", "true");
   response.headers.set("Access-Control-Allow-Origin", "https://iis.lufy.cz");
-  response.cookies.delete("jwt");
+  response.cookies.delete({ name: "jwt", domain: "iis.lufy.cz" });
   return response;
 }
