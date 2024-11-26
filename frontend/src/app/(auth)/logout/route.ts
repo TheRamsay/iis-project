f5ef94da8add8c91ff4690c60117e6298a42fa49
LufyCZ <me@lufy.cz>
2024-11-26 14:15:46 +0000

    cookei fix

--- frontend/src/app/(auth)/logout/route.ts
+++ frontend/src/app/(auth)/logout/route.ts
@@ -5,6 +5,8 @@ export const dynamic = "force-dynamic";
 export function GET() {
   const response = new NextResponse(null, { status: 303 });
   response.headers.set("Location", "/");
+  response.headers.set("Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Origin", "https://iis.lufy.cz");
   response.cookies.delete("jwt");
   return response;
 }
