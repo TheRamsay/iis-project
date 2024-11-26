28caafbe4f621662ce8b1000c21086c505649d10
LufyCZ <me@lufy.cz>
2024-11-26 14:11:20 +0000

    delete cookie

--- frontend/src/app/(auth)/logout/route.ts
+++ frontend/src/app/(auth)/logout/route.ts
@@ -5,6 +5,6 @@ export const dynamic = "force-dynamic";
 export function GET() {
   const response = new NextResponse(null, { status: 303 });
   response.headers.set("Location", "/");
-  response.cookies.set("jwt", "", { expires: new Date(0) });
+  response.cookies.delete("jwt");
   return response;
 }
